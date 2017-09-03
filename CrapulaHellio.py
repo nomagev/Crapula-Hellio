7f9266cd4c185c1ba3f935188d503ac0e12d2d76
Nomagev <vegamontesino@msn.com>
2017-09-04 00:38:56 +0200

    Some pylint work

--- CrapulaHellio.py
+++ CrapulaHellio.py
@@ -11,25 +11,33 @@ __license__ = "GPL 2.0"
 __email__ = "vegamontesino@msn.com"
 __status__ = "Development"
 
+# -*- coding: utf-8 -*-
 
 # Import urllib (request, parse) and re library modules
-import urllib.request
-#import urllib.parse
-#import re
+# Print Import Warning in case of Issue
+try:
+    import urllib.request
+    #import urllib.parse
+    #import re
+    import sys
+except ImportWarning as warning:
+    print(str(warning))
+    sys.exit()
 
 # Enter URL
-webURL = input("Enter Full URL: ")
-url = webURL
+WEB_URL = input("Enter Full URL: ")
+url = WEB_URL
 
 # Create flow to get website (if error, message will be displayed)
 try:
     webHeaders = {}
+    # pylint: disable=line-too-long
     webHeaders['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    # pylint: enable=line-too-long
     webRequest = urllib.request.Request(url, headers=webHeaders)
     webResponse = urllib.request.urlopen(url)
     webData = webResponse.read()
     print(webData)
-
 except Exception as error:
     print(str(error))
 
@@ -40,3 +48,5 @@ except Exception as error:
 # Get the headlines from the indicated site
 #for allHeadlines in headlines:
 #    print(allHeadlines)
+
+# End of Program
