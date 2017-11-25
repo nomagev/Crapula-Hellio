0a952b263d80a8472574d944a4de0d69542e768c
Nomagev <vegamontesino@msn.com>
2017-11-25 13:01:02 +0100

    Pushing changes

--- CrapulaHellio.py
+++ CrapulaHellio.py
@@ -17,8 +17,8 @@ __status__ = "Development"
 # Print Import Warning in case of Issue
 try:
     import urllib.request
-    #import urllib.parse
-    #import re
+    import urllib.parse
+    import re
     import sys
 except ImportWarning as warning:
     print(str(warning))
@@ -31,23 +31,23 @@ url = "http://" + WEB_URL
 # Create flow to get website's HTML Code
 # (if error, message will be displayed)
 try:
-    webHeaders = {}
+    webheaders = {}
     # pylint: disable=line-too-long
-    webHeaders['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    webheaders['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
     # pylint: enable=line-too-long
-    webRequest = urllib.request.Request(url, headers=webHeaders)
-    webResponse = urllib.request.urlopen(url)
-    webData = webResponse.read()
-    print(webData)
+    webrequest = urllib.request.Request(url, headers=webheaders)
+    webresponse = urllib.request.urlopen(url)
+    webdata = webresponse.read()
+    print(webdata)
 except Exception as error:
     print(str(error))
 
 
 # Find text between HTML "headline" tags
-#headlines = re.findall(r'<h1>(.*?)</h1>', str(respData))
+headlines = re.findall(r'<h1>(.*?)</h1>', str(respData))
 
 # Get the headlines from the indicated site
-#for allHeadlines in headlines:
-#    print(allHeadlines)
+for allheadlines in headlines:
+    print(allheadlines)
 
 # End of Program
